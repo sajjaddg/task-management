961847e3422566fcc54ce1787f957800f76537a7
sajjaddg <sajjaddg@gmail.com>
2022-07-12 01:39:20 +0430

    fix responsive

--- src/component/Button/AddProject/AddProject-Button-component.jsx
+++ src/component/Button/AddProject/AddProject-Button-component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AddButtoncircle = ()=>{
     return(
         <button onClick={()=>{}} className="border-2 border-[#D9E0E8] w-14 h-14 bg-[#EDF2F8] rounded-full flex justify-center items-center border-dashed">
-            <h1 className="text-3xl text-[#5D68FE]">+</h1>
+            <h1 className="text-3xl text-[#5D68FE] pb-1">+</h1>
         </button>
     )
 }
--- src/screen/Task/Task-Screen.jsx
+++ src/screen/Task/Task-Screen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AddButtoncircle from "../../component/Button/AddProject/AddProject-Button-component";
-import ProjectTitle from "../../component/ProjectTitle/Project-Title-componenet";
+import ProjectTitle from "../../component/ProjectTitle/Project-Title-component";
 const TaskScreen =()=>{
     return(
         <div className="p-10">
@@ -8,16 +8,16 @@ const TaskScreen =()=>{
                 <h1 className="text-5xl">Task</h1>
                 <AddButtoncircle/>
            </div>
-           <div className="flex flex-wrap flex-col items-center md:space-y-0 space-y-4 md:space-x-4 md:flex-row md:justify-center px-2 mt-4">
-               <ProjectTitle title={'Backlog'}/>
-               <ProjectTitle title={'To Do'}/>
-               <ProjectTitle title={'In Progress'}/>
-               <ProjectTitle title={'Review'}/>
-               <ProjectTitle title={'Backlog'}/>
-               <ProjectTitle title={'To Do'}/>
-               <ProjectTitle title={'In Progress'}/>
-               <ProjectTitle title={'Review'}/>
-           </div>
+            <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-4 mt-4">
+                <ProjectTitle title={'Backlog'}/>
+                <ProjectTitle title={'To Do'}/>
+                <ProjectTitle title={'In Progress'}/>
+                <ProjectTitle title={'Review'}/>
+                <ProjectTitle title={'Backlog'}/>
+                <ProjectTitle title={'To Do'}/>
+                <ProjectTitle title={'In Progress'}/>
+                <ProjectTitle title={'Review'}/>
+            </div>
         </div>
     )
 }
